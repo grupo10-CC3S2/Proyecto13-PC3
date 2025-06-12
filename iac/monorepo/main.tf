cce57ea253f73169e6c48cfc5a60b14a6a876883
Alex Vega <axvg@users.noreply.github.com>
2025-06-11 22:32:04 -0500

    feat(tf): agrega main.tf que importa los modulos para creacion de recursos compute, network y storage

--- iac/monorepo/main.tf
+++ iac/monorepo/main.tf
@@ -4,3 +4,19 @@ module "network" {
   name = "network-1"
   port = 8080
 }
+
+module "compute" {
+  source = "./compute_module"
+
+  instance_count = 4
+  name = "server-1"
+  network_name = module.network.network_name_out
+}
+
+module "db" {
+  source = "./storage_module"
+
+  name = "db-1"
+  server_name = module.compute.server_name_out
+  network_name = module.network.network_name_out
+}
