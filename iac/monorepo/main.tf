eb27c7e32f53a4a4df357dfb5e074fdf81e801f6
Alex Vega <axvg@users.noreply.github.com>
2025-06-13 17:59:11 -0500

    feat(tf): declara dependencia entre modulos

--- iac/monorepo/main.tf
+++ iac/monorepo/main.tf
@@ -9,14 +9,18 @@ module "compute" {
   source = "./compute_module"
 
   instance_count = 4
-  name = "server-1"
-  network_name = module.network.network_name_out
+  name           = "server-1"
+  network_name   = module.network.network_name_out
+
+  depends_on = [module.network]
 }
 
 module "db" {
   source = "./storage_module"
 
-  name = "db-1"
-  server_name = module.compute.server_name_out
+  name         = "db-1"
+  server_name  = module.compute.server_name_out
   network_name = module.network.network_name_out
+
+  depends_on = [module.network, module.compute]
 }
