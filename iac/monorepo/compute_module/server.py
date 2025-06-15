ee10a3bcdea169b50403f5b00612598848969d9b
Alex Vega <axvg@users.noreply.github.com>
2025-06-14 22:13:20 -0500

    feat(py): agrega instancias para crear multiples servidores

--- iac/monorepo/compute_module/server.py
+++ iac/monorepo/compute_module/server.py
@@ -1,8 +1,9 @@
 class ServerFatoryModule:
-    def __init__(self, name, network):
+    def __init__(self, name, network, instance_count=1):
         self.name = name
         self.network = network
         self.server_type = "web"
+        self.instance_count = instance_count
 
     def build(self):
         command = f"echo 'Creando server {self.name} con network {self.network.name}'"
@@ -11,6 +12,7 @@ class ServerFatoryModule:
             {
                 "null_resource": {
                     self.name: {
+                        "count": self.instance_count,
                         "provisioner": {"local-exec": {"command": command}},
                     },
                 }
