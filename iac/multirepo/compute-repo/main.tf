54162ddbab5adb61b4d2c8da7c1280b211746819
Chriss5-2 <christiangiovannixd@gmail.com>
2025-06-19 06:20:37 -0500

    feat(multirepo): Añadir scripts rollback.sh y simular_drift.sh

--- iac/multirepo/compute-repo/main.tf
+++ iac/multirepo/compute-repo/main.tf
@@ -1,4 +1,7 @@
 resource "null_resource" "server-1" {
+  triggers = {
+    version = "v1"
+  }
   provisioner "local-exec" {
     command = "echo 'Creando server ${var.name} con network ${var.network_name}'"
   }
